e389687649f4c212b8bf6230fa9795d1f7e85612
pangho <pang-pangho>
2024-08-19 19:23:37 +0900

    모바일 메뉴 대응

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -1,11 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faUser } from "@fortawesome/free-regular-svg-icons";
-import { faSearch, faBars } from "@fortawesome/free-solid-svg-icons";
+import {
+  faUser,
+  faBars,
+  faTimes,
+  faSearch,
+} from "@fortawesome/free-solid-svg-icons";
 import { useNavigate } from "react-router-dom";
-import { Dropdown, DropdownButton } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
+
 const Navbar = ({ authenticate, setAuthenticate }) => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
   const navigate = useNavigate();
 
   const goToLogin = () => {
@@ -32,53 +37,49 @@ const Navbar = ({ authenticate, setAuthenticate }) => {
     "Women",
     "Men",
     "Baby",
-
     "H&M HOME",
     "Sport",
     "Sale",
     "지속가능성",
   ];
 
+  const toggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen);
+  };
+
   return (
     <div>
       <div className="login-button">
-        <FontAwesomeIcon icon={faUser} />
-        <div className="auth" onClick={() => goToLogin()}>
+        <div className="menu mobile" onClick={toggleSidebar}>
+          <FontAwesomeIcon icon={faBars} />
+        </div>
+        <div className="auth" onClick={goToLogin}>
+          <FontAwesomeIcon icon={faUser} />
           {authenticate ? "로그아웃" : "로그인"}
         </div>
       </div>
-      <div className="nav-section">
+      <div className={`sidebar ${isSidebarOpen ? "open" : ""}`}>
+        <div className="sidebar-header">
+          <FontAwesomeIcon icon={faTimes} onClick={toggleSidebar} />
+        </div>
+        <ul className="sidebar-menu">
+          {menuList.map((menu, index) => (
+            <li key={index}>{menu}</li>
+          ))}
+        </ul>
+      </div>
+
+      <div className="search-box">
+        <FontAwesomeIcon icon={faSearch} />
+        <input type="text" onKeyDown={search} placeholder="검색" />
+      </div>
+      <div className="logo">
         <img
-          onClick={() => goToMain()}
+          onClick={goToMain}
           width={100}
           src="https://i.pinimg.com/564x/60/30/0c/60300c52085ba4c70ec47d3183709538.jpg"
           alt="H&M"
         />
-      </div>
-      <div className="menu-area">
-        <div className="menu-container">
-          {/* PC 화면 */}
-          <ul className="menu-list pc">
-            {menuList.map((menu, index) => (
-              <li key={index}>{menu}</li>
-            ))}
-          </ul>
-          {/* 모바일 화면 */}
-          <DropdownButton
-            title={<FontAwesomeIcon icon={faBars} />}
-            className="menu mobile"
-          >
-            {menuList.map((menu, index) => (
-              <Dropdown.Item as="button" key={index}>
-                {menu}
-              </Dropdown.Item>
-            ))}
-          </DropdownButton>
-          <div className="search-box">
-            <FontAwesomeIcon icon={faSearch} />
-            <input type="text" onKeyDown={(event) => search(event)} />
-          </div>
-        </div>
       </div>
     </div>
   );
